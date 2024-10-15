b312fe032c9522905be62fe471c56e47b0e6e111
Gabi <161313524+DESPIKABLEEE@users.noreply.github.com>
2024-10-15 10:23:12 +0200

    Add files via upload

---
+++ vjezba1/zad1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+	int a, b, s;
+	std::cout << "Unesi dva broja \n"; //<< std::endl;
+	std::cin >> a >> b;
+	s = a + b;
+	std::cout << "suma je" << s << std::endl;
+	std::cout << "suma je" << a + b << std::endl;
+}
